1fe0145b23e2cb993b246b70ae318241dd39b87b
omerfdev <omerfdev@gmail.com>
2023-12-21 06:50:46 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,37 @@
-import './App.css';
-import Dashboard from './modules/Dashboard';
-import Form from './modules/Form';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children, auth=false }) => {
-  const isLoggedIn = localStorage.getItem('user:token') !== null || false;
-
-  if(!isLoggedIn && auth) {
-    return <Navigate to={'/users/sign_in'} />
-  }else if(isLoggedIn && ['/users/sign_in', '/users/sign_up'].includes(window.location.pathname)){
-    console.log('object :>> ');
-    return <Navigate to={'/'} />
-  }
-
-  return children
-}
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Messenger from "./pages/messenger/Messenger";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
-    <Routes>
-      <Route path='/' element={
-        <ProtectedRoute auth={true}>
-          <Dashboard/>
-        </ProtectedRoute>
-      } />
-      <Route path='/users/sign_in' element={
-      <ProtectedRoute>
-        <Form isSignInPage={true}/>
-      </ProtectedRoute>
-      } />
-      <Route path='/users/sign_up' element={
-        <ProtectedRoute>
-        <Form isSignInPage={false}/>
-      </ProtectedRoute>
-      } />
-    </Routes>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/messenger">
+          {!user ? <Redirect to="/" /> : <Messenger />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
